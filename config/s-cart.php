af9272200e747b1e1bbed0efd2207f465789e284
S-Cart <giaiphap247.com@gmail.com>
2022-02-13 17:23:31 +0700

    Fix process custom field

--- config/s-cart.php
+++ config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'version'     => '6.8',
-    'sub-version' => '6.8.7',
+    'sub-version' => '6.8.8',
     'type'        => 'basic',
     'homepage'    => 'https://s-cart.org',
     'title'       => 'Free Open Source eCommerce for Business',
